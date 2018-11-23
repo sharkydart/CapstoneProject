6274f0211a31a5b4fe7df736e39b1691125de5be
Curtis <sharkydart@gmail.com>
2018-11-23 05:23:35 -0500

    Have not yet proven access to Cloud Firestore

--- AnyoneCanFish/app/src/main/java/com/homebrewforlife/sharkydart/anyonecanfish/MapsActivity.java
+++ AnyoneCanFish/app/src/main/java/com/homebrewforlife/sharkydart/anyonecanfish/MapsActivity.java
@@ -1,9 +1,15 @@
 package com.homebrewforlife.sharkydart.anyonecanfish;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
@@ -13,6 +19,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,28 +31,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private int RC_SIGN_IN = 0;   //request code
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        mContext = this;
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         if(mapFragment != null)
             mapFragment.getMapAsync(this);
 
-        tryFirebaseSignIn();
+        //immediately try to sign the user in via FirebaseUI
+        FirebaseSignIn();
     }
 
-    private void tryFirebaseSignIn()
-    {
+    private void FirebaseSignOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(mContext,"You have been signed out.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    private void FirebaseSignIn(){
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
                 new AuthUI.IdpConfig.GoogleBuilder().build());
 
-// Create and launch sign-in intent
+        // Create and launch sign-in intent
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
@@ -65,8 +84,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 // ...
                 try {
-                    if(user != null)
-                        Log.d("fart", "Name: " + user.getDisplayName() + " Email: " + user.getEmail() + " UID: " + user.getUid());
+                    if(user != null) {
+                        Log.d("fart", "Name: " + user.getDisplayName() + " Email: " + user.getEmail() + " UID: " + user.getUid() + "IDTOKEN: " + user.getIdToken(true));
+
+                    }
                 }
                 catch(NullPointerException np){
                     np.printStackTrace();
@@ -80,6 +101,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -92,10 +114,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    //menu stuff
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("fart","-make options menu-");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.rootoptions, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_open_settings) {
+            return true;
+        }
+        else if(id == R.id.action_firebase_signout){
+            //disable the menu button
+            item.setEnabled(false);
+            //sign out of Firebase
+            FirebaseSignOut();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
